7c945c63ff9582cc9b48739f98c77b951eabc16d
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-02-17 16:33:31 -0300

    nova classe

--- Main.java
+++ Main.java
@@ -1,23 +1,9 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        DescontoIR desconto = new DescontoIR(4800);
 
-        System.out.print("Informe o coeficiente a: ");
-        double a = Double.parseDouble(scan.nextLine());
+        double salarioLiquido = desconto.salarioLiquido(desconto.salarioBruto);
 
-        System.out.print("Informe o coeficiente b: ");
-        double b = Double.parseDouble(scan.nextLine());
-
-        System.out.print("Informe o coeficiente c: ");
-        double c = Double.parseDouble(scan.nextLine());
-
-        scan.close();
-
-        EquacaoSGrau equacao = new EquacaoSGrau(a, b, c);
-
-        double delta = equacao.calcularDelta();
-        equacao.calcularRaizes(delta);
+        System.out.println(salarioLiquido);
     }
 }
