e44f3a2f689a99ee4c4c8976b53fc94ee4eb3805
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-03-03 16:14:37 -0300

    ajuste

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
-import obj.Caixa;
-import obj.Objeto;
-import obj.Personagem;
+import obj.*;
 
 public class Main {
     public static void main(String[] args) {
-        Objeto[] cenario = new Objeto[] {
-			new Personagem("Frodo", 1, 1),
-			new Personagem("Bilbo", 2, 1),
-			new Caixa(1, 2, 1, 1)
-		};
+        Scene scene = new Scene();
 
 		Scanner scanner = new Scanner(System.in);
 
-		int n = cenario.length - 1;
+		int n = scene.getCenario().length - 1;
+
 		while (true) {
-			System.out.println("Escolha um objeto de 0 a " + n + ":");
+			System.out.print("Escolha um objeto de 0 a " + n + ": ");
 			int i = Integer.parseInt(scanner.nextLine());
-			System.out.println(cenario[i]);
+			System.out.println(scene.getCenario()[i]);
 		}
 
     }
